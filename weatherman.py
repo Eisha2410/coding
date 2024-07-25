0fbf624edde38f27caa645b51307a639319a7c16
Eisha2410 <eraazia496@gmail.com>
2024-07-25 18:46:04 +0500

    comment removed

--- weatherman.py
+++ weatherman.py
@@ -13,23 +13,7 @@ def func_access(file_name):
         my_weatherlist.append(read[i].split())
         print(read[i].split())
     print(read[0])
-    """
-    ptr = 1
-    with open(file_name , "w") as re:
-        for line in read:
-            if ptr != 1:
-                re.write(line)
-            ptr += 1
-    for line in read:
-        my_weatherlist.append(line.split())
-        print(line.split())
-        file.seek(0)
-        file.remove(0)
-        file.remove(1)
-        file.remove(len-1)
-        file.writeline(line[2:range(len-2)])
-        print(line.split())
-    """  
+
 my_file = "weatherdata/lahore_weather_2002_Feb.txt"
 func_access(my_file)
 

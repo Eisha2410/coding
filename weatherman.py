cb63afcad18f8f8b01d57ebd4efca751fcf85c0e
Eisha2410 <eraazia496@gmail.com>
2024-07-29 13:27:46 +0500

    changes done as requested

--- weatherman.py
+++ weatherman.py
@@ -10,11 +10,10 @@ my_weatherlist = []
 
 def read_files(file_name):
     file = open (file_name , "r")
-    read = file.read().splitlines()
+    read = file.read().splitlines(",")
     for i in range(2, len(read)-1):
-        my_weatherlist.append(read[i].split())
-        print(read[i].split())
-    print(read[0])
+        my_weatherlist.append(read[i].split(","))
+        read[i].split(",")
 
 directory = './weatherdata'
 pattern = os.path.join(directory, '*2002*.txt')
@@ -25,22 +24,26 @@ global_min_temp = float('inf')
 highest_humidity = float('-inf')
 
 for file_path in files:
-    print(f'processing file: {file_path}')
+    file_path
     try:
         read_files(file_path)
     except Exception as e:
-        print(f'error processing file {file_path}: {e}')
+        file_path: {e}
 
 for line in my_weatherlist:
-    temperature = float(line[0])  
-    humidity = float(line[1])
-    if temperature > global_max_temp:
-        global_max_temp = temperature
-    if temperature < global_min_temp:
-        global_min_temp = temperature
-                
-    if humidity > highest_humidity:
-        highest_humidity = humidity   
+    try:
+        temperature = float(line[1])  
+        humidity = float(line[1])
+        if temperature > global_max_temp:
+           global_max_temp = temperature
+        if temperature < global_min_temp:
+           global_min_temp = temperature
+        if humidity > highest_humidity:
+           highest_humidity = humidity  
+
+    except (ValueError, IndexError) as e:
+        line: {e}
+        continue
 
 print(f'Maximum Temperature: {global_max_temp}')
 print(f'Minimum Temperature: {global_min_temp}')

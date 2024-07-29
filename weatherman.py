6515a3ec6c29c097cebf23ffcb2df8b646de1239
Eisha2410 <eraazia496@gmail.com>
2024-07-29 16:28:43 +0500

    more correct one program by command line

--- weatherman.py
+++ weatherman.py
@@ -1,8 +1,12 @@
 import os
+import glob
 import sys
 
+sys.argv
 for w in sys.argv:
     print(w)
+if len(sys.argv) < 3:
+    sys.exit(1)
 
 my_weatherlist = []
 
@@ -13,11 +17,17 @@ def read_files(file_name):
         my_weatherlist.append(read[i].split(","))
         read[i].split(",")
 
-files = sys.argv[1:]
+directory = sys.argv[1]
+year = sys.argv[3] if len(sys.argv) > 3 and sys.argv[2] == "-e" else None
+
+pattern = os.path.join(directory, '*2002*.txt')
+files = glob.glob(pattern)
 
 global_max_temp = float('-inf')
 global_min_temp = float('inf')
 highest_humidity = float('-inf')
+humidity_count = 0
+humidity_percentage = 0
 
 for file_path in files:
     if os.path.exists(file_path):
@@ -29,20 +39,23 @@ for file_path in files:
 for line in my_weatherlist:
     try:
         temperature = float(line[1])  
-        humidity = float(line[-1])
+        humidity = float(line[1])
         if temperature > global_max_temp:
            global_max_temp = temperature
         if temperature < global_min_temp:
            global_min_temp = temperature
-
         if humidity > highest_humidity:
-           highest_humidity = humidity
-           humidity = (humidity * highest_humidity)/ 100.0
+           highest_humidity = humidity 
+
+        humidity_percentage += humidity
+        humidity_count += 1
+        humidity_percentage = (humidity/highest_humidity)* 100.0
+
 
     except (ValueError, IndexError) as e:
         line: {e}
         continue
 
-print(f'Highhest: {global_max_temp}')
+print(f'Highest: {global_max_temp}')
 print(f'Lowest: {global_min_temp}')
-print(f'Humidity: {highest_humidity}')
+print(f'Humidity: {humidity_percentage}')

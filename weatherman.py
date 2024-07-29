b48407d5a3cc69de0499b45f393e1f2c12ea1aa1
Eisha2410 <eraazia496@gmail.com>
2024-07-29 14:26:27 +0500

    files accessed by command line argument

--- weatherman.py
+++ weatherman.py
@@ -1,8 +1,6 @@
 import os
-import glob
 import sys
 
-sys.argv
 for w in sys.argv:
     print(w)
 
@@ -15,36 +13,36 @@ def read_files(file_name):
         my_weatherlist.append(read[i].split(","))
         read[i].split(",")
 
-directory = './weatherdata'
-pattern = os.path.join(directory, '*2002*.txt')
-files = glob.glob(pattern)
+files = sys.argv[1:]
 
 global_max_temp = float('-inf')
 global_min_temp = float('inf')
 highest_humidity = float('-inf')
 
 for file_path in files:
-    file_path
-    try:
-        read_files(file_path)
-    except Exception as e:
-        file_path: {e}
+    if os.path.exists(file_path):
+        try:
+            read_files(file_path)
+        except Exception as e:
+            file_path: {e}
 
 for line in my_weatherlist:
     try:
         temperature = float(line[1])  
-        humidity = float(line[1])
+        humidity = float(line[-1])
         if temperature > global_max_temp:
            global_max_temp = temperature
         if temperature < global_min_temp:
            global_min_temp = temperature
+
         if humidity > highest_humidity:
-           highest_humidity = humidity  
+           highest_humidity = humidity
+           humidity = (humidity * highest_humidity)/ 100.0
 
     except (ValueError, IndexError) as e:
         line: {e}
         continue
 
-print(f'Maximum Temperature: {global_max_temp}')
-print(f'Minimum Temperature: {global_min_temp}')
-print(f'Highest Humidity: {highest_humidity}')
+print(f'Highhest: {global_max_temp}')
+print(f'Lowest: {global_min_temp}')
+print(f'Humidity: {highest_humidity}')

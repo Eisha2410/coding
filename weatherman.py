79927be81440c1cc2b65b98ac25d86b0ce6134df
Eisha2410 <eraazia496@gmail.com>
2024-07-30 16:33:10 +0500

    issues addresed and date also added

--- weatherman.py
+++ weatherman.py
@@ -8,6 +8,9 @@ for w in sys.argv:
 if len(sys.argv) < 3:
     sys.exit(1)
 
+years = sys.argv[2]
+sys.argv[3] if len(sys.argv) > 3 and sys.argv[2] == "-e" else None
+
 my_weatherlist = []
 
 def read_files(file_name):
@@ -18,10 +21,7 @@ def read_files(file_name):
         read[i].split(",")
 
 directory = "./weatherdata"
-year = sys.argv[1]
-sys.argv[3] if len(sys.argv) > 3 and sys.argv[2] == "-e" else None
-
-pattern = os.path.join(directory, year )
+pattern = os.path.join(directory, f"{years}*" )
 files = glob.glob(pattern)
 
 global_max_temp = float('-inf')
@@ -29,6 +29,10 @@ global_min_temp = float('inf')
 highest_humidity = float('-inf')
 humidity_count = 0
 humidity_percentage = 0
+max_temp_date = ""
+min_temp_date = ""
+humidity_date = ""
+
 
 for file_path in files:
     if os.path.exists(file_path):
@@ -40,13 +44,19 @@ for file_path in files:
 for line in my_weatherlist:
     try:
         temperature = float(line[1])  
-        humidity = float(line[1])
+        humidity = float(line[-1])
+        date_str = line[0]
+        year, month, day = date_str.split('-') 
+
         if temperature > global_max_temp:
            global_max_temp = temperature
+           max_temp_date = date_str
         if temperature < global_min_temp:
            global_min_temp = temperature
+           min_temp_date = date_str
         if humidity > highest_humidity:
            highest_humidity = humidity 
+           humidity_date = date_str
 
         humidity_percentage += humidity
         humidity_count += 1
@@ -57,8 +67,6 @@ for line in my_weatherlist:
         line: {e}
         continue
 
-print(f'Highest: {global_max_temp}')
-print(f'Lowest: {global_min_temp}')
-print(f'Humidity: {humidity_percentage}')
-
-print("end of file processing")
+print(f'Highest: {global_max_temp} on {max_temp_date}')
+print(f'Lowest: {global_min_temp} on {min_temp_date}')
+print(f'Humidity: {humidity_percentage} on {humidity_date}')

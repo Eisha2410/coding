1f309557faf3d651e0ac7f0717ce3779a4c8168c
Eisha2410 <eraazia496@gmail.com>
2024-08-06 16:44:26 +0500

    complete task2 with the improved requested

--- weatherman.py
+++ weatherman.py
@@ -8,16 +8,15 @@ def parse_arguments():
         sys.exit(1)
 
     flag = sys.argv[1]
-    years_month = []
-    if not len(sys.argv) == 3:
+    if flag not in['-e', '-a']: 
         sys.exit(1)
+    
+    years_month = sys.argv[2:]
+    if flag == '-e' and len(years_month) != 1:
+        sys.exit("error: -e flag requires only one year")
+    elif flag == '-a'and len(years_month) != 1:
+        sys.exit("error: -a flag requires one year/month ")
 
-    if flag == '-e':
-        years_month = [sys.argv[2]]
-    elif flag == '-a':
-        years_month = sys.argv[2:]
-    else:
-        sys.exit(1)
     return flag, years_month
 
 def map_months(years_month): 
@@ -36,8 +35,7 @@ def map_months(years_month):
             years.append(year)
             months[year] = month_symbol[month]
         else:
-            year = ym
-            years.append(year)
+            sys.exit("error: -a flag requires year/month in argument")
     return years, months
 
 def read_files(file_name, my_weatherlist):
@@ -47,7 +45,13 @@ def read_files(file_name, my_weatherlist):
         my_weatherlist.append(read[i].split(","))
         read[i].split(",")
 
-def data_execution(my_weatherlist):
+def data_execution(my_weatherlist, flag):
+    if flag == '-e':
+        execute_e_argument(my_weatherlist)
+    elif flag == '-a':
+        execute_a_argument(my_weatherlist)
+
+def execute_e_argument(my_weatherlist):
     global_max_temp = float('-inf')
     global_min_temp = float('inf')
     highest_humidity = float('-inf')
@@ -99,22 +103,65 @@ def data_execution(my_weatherlist):
     print(f'Lowest: {global_min_temp} on {min_temp_str}')
     print(f'Humidity: {humidity_percentage} on {humidity_date_str}')
 
+def execute_a_argument(my_weatherlist):
+    temp_dict = {}
+    for line in my_weatherlist:
+        try:
+            date_str = line[0]
+            temperature = float(line[1])
+            humidity = float(line[-1])
+            date_obj = datetime.datetime.strptime(date_str, "%Y-%m-%d")
+            year = date_obj.year
+
+            if year not in temp_dict:
+                temp_dict[year] = {'temp' : [], 'humidities' : []}
+
+            temp_dict[year]['temp'].append(temperature)
+            temp_dict[year]['humidities'].append(humidity)
+        except (ValueError, IndexError) as e:
+            continue
+    
+    highest_avg_temp = float('-inf')
+    lowest_avg_temp = float('inf')
+    total_avg_humidity = 0
+    year_count = 0
+
+    for year in temp_dict:
+        avg_temp = sum(temp_dict[year]['temp']) / len(temp_dict[year]['temp'])
+        avg_humidity = sum(temp_dict[year]['humidities']) / len(temp_dict[year]['humidities'])
+
+        if avg_temp > highest_avg_temp:
+            highest_avg_temp = avg_temp
+        if avg_temp < lowest_avg_temp:
+            lowest_avg_temp = avg_temp
+
+        total_avg_humidity += avg_humidity
+        year_count += 1
+
+    avg_humidity_percentage = (total_avg_humidity / year_count)
+
+    print(f'Highest Average Temperature: {round(highest_avg_temp)}')
+    print(f'Lowest Average Temperature: {round(lowest_avg_temp)}')
+    print(f'Average Mean Humidity Percentage: {avg_humidity_percentage:.2f}%')
+
+
 def handling_e_command(year_month):
-    years, months = map_months(year_month)
-    process_files(years, months)
+    years = year_month
+    months = {}
+    process_files(years, months, '-e')
     print("command e")
 
 def handling_a_command(years_month):
     years, months = map_months(years_month)
-    process_files(years, months)
+    process_files(years, months, '-a')
     print("command a")
 
-def process_files(years, months):
+def process_files(years, months, flag):
     directory = "./weatherdata"
     files = []
     for year in years:
-        if year in months:
-            pattern = os.path.join(directory, f"lahore_weather_{year}_{months[year]}*")
+        if flag == '-a' and year in months:
+                pattern = os.path.join(directory, f"lahore_weather_{year}_{months[year]}*")
         else:
             pattern = os.path.join(directory, f"lahore_weather_{years}*")
         files.extend(glob.glob(pattern))
@@ -128,11 +175,10 @@ def process_files(years, months):
             except Exception as e:
                 file_path: {e}
 
-    data_execution(my_weatherlist)
+    data_execution(my_weatherlist, flag)
 
 def main():
     flag, years_months = parse_arguments()
-    print(years_months)
     
     if flag == '-e':
         handling_e_command(years_months)
